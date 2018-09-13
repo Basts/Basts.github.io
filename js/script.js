41938f40f13d53783bcdc9f9689eae18f26efbb2
Basts <38281922+Basts@users.noreply.github.com>
2018-09-13 21:29:40 +0200

    My solution
    My solution

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let answers = [];
+
+function showAndHideElements (currentElement, nextElement) {
+  document.getElementById(currentElement).classList.remove('active');
+  document.getElementById(nextElement).classList.add('active');
+}
+
+function answerQuestion (currentElement, nextElement, answer) {
+  showAndHideElements(currentElement, nextElement);
+  answers.push(answer);
+}
+
+function answerLastQuestion (currentElement, answer) {
+  answers.push(answer);
+
+  const isValid = answers.filter(value => value === false).length === 0;
+
+  if (isValid) {
+    showAndHideElements(currentElement, 'cv');
+  } else {
+    showAndHideElements(currentElement, 'warning');
+  }
+}
+
+
+function showAndHideElementsReset (currentElement, nextElement) {
+  document.getElementById(currentElement).classList.remove('active');
+  document.getElementById(nextElement).classList.add('active');
+  answers.length = [];
+
+}
